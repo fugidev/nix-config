f9cd573172f7372528f483bdcefaa3e5ad13c402
Fugi <me@fugi.dev>
2023-07-31 00:50:38 +0200

    librarian/jellyfin: init

---
+++ hosts/librarian/jellyfin.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+{
+  services.jellyfin.enable = true;
+
+  # mount media paths
+  systemd.services.jellyfin.serviceConfig.BindReadOnlyPaths = [
+    "/data/share/Filme:/data/movies"
+    "/data/share/Serien:/data/tvshows"
+    "/data/share/Hörbücher:/data/audiobooks"
+  ];
+
+  # auto discovery
+  networking.firewall.allowedUDPPorts = [ 1900 7359 ];
+
+  services.nginx.virtualHosts."jellyfin.${config.fugi.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.fugi.domain;
+
+    extraConfig = ''
+      # The default `client_max_body_size` is 1M, this might not be enough for some posters, etc.
+      client_max_body_size 20M;
+
+      # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+      proxy_buffering off;
+    '';
+
+    locations."/" = {
+      proxyPass = "http://localhost:8096";
+      proxyWebsockets = true;
+    };
+  };
+}

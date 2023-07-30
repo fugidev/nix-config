3c3c0da46f8ab55f2bb0b38a39d575cd1bc192ef
Fugi <me@fugi.dev>
2023-07-31 00:53:47 +0200

    librarian/paperless: adjust config

--- hosts/librarian/paperless.nix
+++ hosts/librarian/paperless.nix
@@ -8,6 +8,7 @@
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
       PAPERLESS_ADMIN_USER = "fugi";
       PAPERLESS_FILENAME_FORMAT = "{created_year}/{created_year}-{created_month}-{created_day} {title}";
+      PAPERLESS_OCR_DESKEW = false; # deskew sometimes messes up, so I do it manually
     };
   };
 
@@ -18,6 +19,10 @@
     locations."/" = {
       proxyPass = "http://127.0.0.1:${builtins.toString config.services.paperless.port}";
       proxyWebsockets = true;
+      extraConfig = ''
+        # allow upload of large documents
+        client_max_body_size 100M;
+      '';
     };
   };
 

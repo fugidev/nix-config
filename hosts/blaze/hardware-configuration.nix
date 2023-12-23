c3f030b1b0955f4edfa354d5a676f2178f253d71
Fugi <me@fugi.dev>
2023-12-23 11:54:04 +0100

    blaze: add swapfile

--- hosts/blaze/hardware-configuration.nix
+++ hosts/blaze/hardware-configuration.nix
@@ -50,7 +50,9 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/var/swapfile"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

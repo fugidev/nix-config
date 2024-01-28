506c561db71dcf726e304f82a427252178bb2ca7
Fugi <me@fugi.dev>
2024-01-29 00:51:50 +0100

    nitwit/invidious: fix patch urls

--- hosts/nitwit/modules/invidious.nix
+++ hosts/nitwit/modules/invidious.nix
@@ -9,15 +9,13 @@ in
       patches = [
         # support for private instances
         (pkgs.fetchpatch {
-          url = "https://web.archive.org/web/20240103152302id_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4259.diff";
+          url = "https://web.archive.org/web/20240103152302if_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4259.diff";
           hash = "sha256-PbaDlE1iRm2oAIl4s4cEyu0hhuVfDzX/W0u36bB0dCQ=";
-          decode = "gunzip";
         })
         # chapters support
         (pkgs.fetchpatch {
-          url = "https://web.archive.org/web/20240107104557id_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4111.diff";
+          url = "https://web.archive.org/web/20240107104557if_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4111.diff";
           hash = "sha256-72Hh7kuNhOBIZ9UVjQZBuD48BusUqsyAcEYoAy4DFW4=";
-          decode = "gunzip";
         })
       ];
     });

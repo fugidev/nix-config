dd707f528683b9dbf0ddf1a3c9bef4a2a702ce10
Fugi <me@fugi.dev>
2024-03-23 01:54:20 +0100

    upgrade-diff: path exists check

--- modules/upgrade-diff.nix
+++ modules/upgrade-diff.nix
@@ -2,16 +2,20 @@
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
-      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+      if [[ -e /run/current-system && -e "$systemConfig" ]]; then
+        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+      fi
 
       NO_FORMAT="\033[0m"
       F_BOLD="\033[1m"
       C_RED="\033[38;5;9m"
 
-      ${pkgs.diffutils}/bin/cmp --silent \
-        <(readlink /run/booted-system/{initrd,kernel,kernel-modules}) \
-        <(readlink $systemConfig/{initrd,kernel,kernel-modules}) \
-        || echo -e "''${F_BOLD}''${C_RED}Kernel version changed, reboot is advised.''${NO_FORMAT}"
+      if [[ -e /run/booted-system && -e "$systemConfig" ]]; then
+        ${pkgs.diffutils}/bin/cmp --silent \
+          <(readlink /run/booted-system/{initrd,kernel,kernel-modules}) \
+          <(readlink $systemConfig/{initrd,kernel,kernel-modules}) \
+          || echo -e "''${F_BOLD}''${C_RED}Kernel version changed, reboot is advised.''${NO_FORMAT}"
+      fi
     '';
   };
 }

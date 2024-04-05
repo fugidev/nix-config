ea2e91860b05b2c7b04b856f88a0391820c34929
Fugi <me@fugi.dev>
2024-04-05 16:27:21 +0200

    use invidious from unstable
    playback was broken.
    also disable the chapters patch since it has conflicts.

--- hosts/nitwit/modules/invidious.nix
+++ hosts/nitwit/modules/invidious.nix
@@ -1,8 +1,18 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 let
   cfg = config.services.invidious;
+  nixpkgs-unstable = inputs.nixpkgs;
 in
 {
+  # Use invidious from unstable branch
+  disabledModules = [ "services/web-apps/invidious.nix" ];
+  imports = [ "${nixpkgs-unstable}/nixos/modules/services/web-apps/invidious.nix" ];
+  nixpkgs.overlays = [
+    (self: super: {
+      inherit (nixpkgs-unstable.legacyPackages."x86_64-linux") invidious http3-ytproxy;
+    })
+  ];
+
   services.invidious = {
     enable = true;
     package = pkgs.invidious.overrideAttrs (_old: {
@@ -13,18 +23,20 @@ in
           hash = "sha256-PbaDlE1iRm2oAIl4s4cEyu0hhuVfDzX/W0u36bB0dCQ=";
         })
         # chapters support
-        (pkgs.fetchpatch {
-          url = "https://web.archive.org/web/20240107104557if_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4111.diff";
-          hash = "sha256-72Hh7kuNhOBIZ9UVjQZBuD48BusUqsyAcEYoAy4DFW4=";
-        })
+        # (pkgs.fetchpatch {
+        #   url = "https://web.archive.org/web/20240107104557if_/https://patch-diff.githubusercontent.com/raw/iv-org/invidious/pull/4111.diff";
+        #   hash = "sha256-72Hh7kuNhOBIZ9UVjQZBuD48BusUqsyAcEYoAy4DFW4=";
+        # })
       ];
     });
     domain = "invidious.${config.fugi.baseDomain}";
     nginx.enable = true;
+    http3-ytproxy.enable = true;
     port = 8723;
     settings = {
       registration_enabled = false;
       private_instance = true;
+      db.user = "invidious";
     };
   };
 

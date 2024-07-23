cea91d304da6172a844fcd5fad2ef57c789a624e
Fugi <me@fugi.dev>
2024-07-23 20:02:48 +0200

    home/alacritty: keybinds/hints

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -11,7 +11,7 @@
         size = 13;
       };
 
-      colors = {
+      colors = rec {
         primary = {
           foreground = "#dddddd";
           background = "#111111";
@@ -37,9 +37,43 @@
           cyan = "#78c5f5";
           white = "#ffffff";
         };
+        hints = {
+          start = {
+            background = normal.yellow;
+            foreground = normal.black;
+          };
+          end = {
+            background = "#a08734";
+            foreground = normal.black;
+          };
+        };
       };
 
       cursor.style.shape = "Beam";
+
+      keyboard.bindings = [{
+        key = "N";
+        mods = "Command | Shift";
+        action = "SpawnNewInstance";
+      }];
+
+      hints.enabled = [{
+        command = "open";
+        hyperlinks = true;
+        post_processing = true;
+        persist = false;
+        mouse = {
+          enabled = true;
+          mods = "Command";
+        };
+        binding = {
+          key = "O";
+          mods = "Command|Shift";
+        };
+        regex = lib.removeSuffix "\n" ''
+          (ipfs:|ipns:|magnet:|mailto:|gemini://|gopher://|https://|http://|news:|file:|git://|ssh:|ftp://)[^\u0000-\u001F\u007F-\u009F<>"\\s{-}\\^⟨⟩`]+
+        '';
+      }];
     };
   };
 

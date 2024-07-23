a0a4692d8a25f411b4c8f583f136a9267111e7bf
Fugi <me@fugi.dev>
2024-07-23 20:05:36 +0200

    darwin: better path declaration

--- modules/home/home-fugi-darwin.nix
+++ modules/home/home-fugi-darwin.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ lib, inputs, ... }:
 {
   imports = [
     ./zsh.nix
@@ -18,19 +18,26 @@
   home.username = "fugi";
   home.homeDirectory = "/Users/fugi";
 
-  programs.zsh = {
-    completionInit = ''
-      fpath+=(/opt/homebrew/share/zsh/site-functions)
-    '';
-    envExtra = ''
-      export PATH="/usr/local/bin:$PATH"
+  programs.zsh =
+    let
+      homebrew = "/opt/homebrew";
+      binDirs = [
+        "/Users/fugi/fvm/default"
+        "${homebrew}/opt/openjdk"
+        "${homebrew}/opt/node@20"
+        # "${homebrew}/opt/node@18"
+      ];
+    in
+    {
+      completionInit = ''
+        fpath+=(${homebrew}/share/zsh/site-functions)
+      '';
+      envExtra = ''
+        export PATH="/usr/local/bin:$PATH"
 
-      eval "$(/opt/homebrew/bin/brew shellenv)"
+        eval "$(${homebrew}/bin/brew shellenv)"
 
-      #export PATH="/opt/homebrew/opt/node@18/bin:$PATH"
-      export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
-      export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-      export PATH="/Users/fugi/fvm/default/bin:$PATH"
-    '';
-  };
+        export PATH="${lib.makeBinPath binDirs}:$PATH"
+      '';
+    };
 }

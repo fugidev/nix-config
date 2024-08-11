29d569a8312a76c4b6a4db111f242d3b74be80fd
Fugi <me@fugi.dev>
2024-08-11 11:49:36 +0200

    sops: fix defaultSopsFile path

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,9 +1,9 @@
-{ machineConfig, inputs, ... }:
+{ machineConfig, inputs, flakeRoot, ... }:
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops = {
-    defaultSopsFile = ./hosts/${machineConfig.hostName}/secrets.yaml;
+    defaultSopsFile = flakeRoot + /hosts/${machineConfig.hostName}/secrets.yaml;
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       generateKey = false;

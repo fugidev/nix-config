9682039556a96c87344a967bfffe1824dbd6685b
Fugi <me@fugi.dev>
2024-12-04 10:46:19 +0100

    yeet birdtray
    it brokey

--- modules/home/mail.nix
+++ modules/home/mail.nix
@@ -1,19 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.thunderbird = {
     enable = true;
-    package = pkgs.thunderbird.overrideAttrs (old: {
-      buildCommand = old.buildCommand + ''
-        wrapProgram $out/bin/thunderbird \
-          --set-default MOZ_ENABLE_WAYLAND 0
-      '';
-    });
     profiles."default" = {
       isDefault = true;
     };
   };
-
-  home.packages = with pkgs; [
-    birdtray
-  ];
 }

9dc50776ea205c01197b2439704d42cc03ea2e84
Fugi <me@fugi.dev>
2025-02-16 13:37:07 +0100

    add home printer

--- modules/printing.nix
+++ modules/printing.nix
@@ -14,11 +14,20 @@ in
     ];
   };
 
-  hardware.printers.ensurePrinters = [{
-    name = "Heiko";
-    description = "Drucker im FSR Buero";
-    deviceUri = "dnssd://Kyocera%20ECOSYS%20M6630cidn._ipp._tcp.local/?uuid=4509a320-007e-002c-00dd-002507504ad0";
-    location = "FSR Buero";
-    model = heikoDriver;
-  }];
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Heiko";
+      description = "Drucker im FSR Buero";
+      deviceUri = "dnssd://Kyocera%20ECOSYS%20M6630cidn._ipp._tcp.local/?uuid=4509a320-007e-002c-00dd-002507504ad0";
+      location = "FSR Buero";
+      model = heikoDriver;
+    }
+    {
+      name = "HP_Color_LaserJet";
+      description = "HP Color LaserJet Pro MFP 3302";
+      deviceUri = "ipp://192.168.0.6/ipp";
+      location = "3D-Drucker-Zimmer";
+      model = "everywhere";
+    }
+  ];
 }

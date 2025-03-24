e30d75860195680615238ef996f79683cfeb1809
Fugi <me@fugi.dev>
2025-03-24 16:53:03 +0100

    use same wallpaper everywhere

--- hosts/blaze/home/home.nix
+++ hosts/blaze/home/home.nix
@@ -1,10 +1,8 @@
-{ pkgs, flakeRoot, ... }:
+{ flakeRoot, ... }:
 {
   imports = [
     (flakeRoot + /modules/home/librewolf.nix)
   ];
-
-  fugi.wallpaper = pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath;
 
   home.pointerCursor.size = 24;
   home.stateVersion = "22.05";
--- modules/home/user-options.nix
+++ modules/home/user-options.nix
@@ -1,8 +1,12 @@
-{ lib, ... }: with lib; {
+{ lib, pkgs, ... }: with lib; {
   options.fugi = {
     wallpaper = mkOption {
       type = types.path;
       description = "desktop and lockscreen background";
+      default = pkgs.fetchurl {
+        url = "https://web.archive.org/web/20230404203951if_/https://images.wallpapersden.com/image/download/macos-12-monterey-digital_bG1mZ2mUmZqaraWkpJRpbW5trWlpamc.jpg";
+        sha256 = "NA4nhCcnT6B9IJQWh8ldnjSt9eUFmte6AfN3cNz8Fwk=";
+      };
     };
 
     guiApps = mkOption {

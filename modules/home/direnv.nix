725d7062417b7b6e8f15e383daa3297e32811b58
Fugi <me@fugi.dev>
2025-03-25 12:25:11 +0100

    direnv: use centralized cache directory

--- modules/home/direnv.nix
+++ modules/home/direnv.nix
@@ -2,6 +2,18 @@
 {
   programs.direnv = {
     enable = true;
+    stdlib = /* sh */ ''
+      # use centralized cache directory
+      # see https://github.com/direnv/direnv/wiki/Customizing-cache-location
+      : ''${XDG_CACHE_HOME:=$HOME/.cache}
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+          echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
+        )}"
+      }
+    '';
     nix-direnv.enable = true;
   };
 

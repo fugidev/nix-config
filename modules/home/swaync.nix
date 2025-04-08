426738fe67259b7e1122d37f350b2d4761917361
Fugi <me@fugi.dev>
2025-04-08 15:27:43 +0200

    swaync: add styles

--- modules/home/swaync.nix
+++ modules/home/swaync.nix
@@ -13,9 +13,30 @@
       ];
     };
 
-    style = ''
+    style = /* css */ ''
       .control-center {
         padding: 16px;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+
+      .notification-group,
+      .notification-row {
+        background-color: transparent;
+        padding-left: 0;
+        padding-right: 0;
+      }
+
+      .notification-content {
+        padding: 8px;
+      }
+
+      .notification-content .time {
+        font-weight: normal;
+      }
+
+      .widget-mpris-player {
+        background-color: rgba(0,0,0,.2);
       }
     '';
   };

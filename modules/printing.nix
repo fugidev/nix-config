091e4cacfe67ba031f37b840b1015b03963a5a57
Fugi <me@fugi.dev>
2025-04-08 14:37:39 +0200

    printing: remove explicit printers

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,33 +1,10 @@
-{ pkgs, ... }:
-let
-  heikoDriver = "Kyocera ECOSYS M6630cidn.ppd";
-in
+{ ... }:
 {
-  services = {
-    avahi.enable = true;
-    avahi.nssmdns4 = true;
-    avahi.openFirewall = true;
-
-    printing.enable = true;
-    printing.drivers = [
-      (pkgs.writeTextDir ("share/cups/model/" + heikoDriver) (builtins.readFile ../misc/${heikoDriver}))
-    ];
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
   };
 
-  hardware.printers.ensurePrinters = [
-    {
-      name = "Heiko";
-      description = "Drucker im FSR Buero";
-      deviceUri = "dnssd://Kyocera%20ECOSYS%20M6630cidn._ipp._tcp.local/?uuid=4509a320-007e-002c-00dd-002507504ad0";
-      location = "FSR Buero";
-      model = heikoDriver;
-    }
-    {
-      name = "HP_Color_LaserJet";
-      description = "HP Color LaserJet Pro MFP 3302";
-      deviceUri = "ipp://[fd00::6]/ipp";
-      location = "3D-Drucker-Zimmer";
-      model = "everywhere";
-    }
-  ];
+  services.printing.enable = true;
 }

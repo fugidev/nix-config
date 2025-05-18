45d252856b42b53935fd68975a6ef1ffc5826278
Fugi <me@fugi.dev>
2025-05-18 12:11:57 +0200

    blaze/zsh: remove rebuild alias override

--- hosts/blaze/modules/zsh.nix
+++ hosts/blaze/modules/zsh.nix
@@ -1,10 +1,9 @@
-{ lib, ... }:
+{ ... }:
 {
   fugi.promptColor = "#f7ce46"; # yellow
 
   programs.zsh = {
     shellAliases = {
-      rebuild = lib.mkForce "nixos-rebuild --log-format multiline --impure --use-remote-sudo";
       rebuild-shepherd = "nixos-rebuild --log-format multiline --target-host shepherd --flake .#shepherd";
     };
   };

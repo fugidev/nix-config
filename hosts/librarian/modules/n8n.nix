f296c3606cf0b15a496034aede63561634346a12
Fugi <me@fugi.dev>
2025-05-30 13:34:43 +0200

    librarian: fix n8n

--- hosts/librarian/modules/n8n.nix
+++ hosts/librarian/modules/n8n.nix
@@ -3,7 +3,10 @@
   services.n8n.enable = true;
 
   services.nginx.virtualHosts."n8n.${config.networking.fqdn}" = {
-    locations."/".proxyPass = "http://localhost:5678";
+    locations."/" = {
+      proxyPass = "http://localhost:5678";
+      proxyWebsockets = true;
+    };
   };
 
   fugi.allowUnfree = [ "n8n" ];

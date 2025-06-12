09aeb15f43694d6752f62a257e40979efbf13aa9
Fugi <me@fugi.dev>
2025-06-12 13:47:45 +0200

    add ssh key

--- modules/options.nix
+++ modules/options.nix
@@ -20,6 +20,7 @@ in
       default = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0X6L7NwTHiOmFzo8mJBCy6H+DKUePAAXU4amm32DAQ fugi@magmacube"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD1ZkrAmC9g5eJPDgv4zuEM+UIIEWromDzM1ltHt4TM fugi@blaze"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLIO/G4CCySDUmi9CQrq05CpWyfTmdv/Y9AJa/IOSNyg9/2hWRD+eL0JeKw+2Z6smH+kRheVVMya2YYckVWjso0="
       ];
     };
 

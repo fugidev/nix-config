db91e7f82da07801f3d56312588c9127e8bf5e5a
Fugi <me@fugi.dev>
2025-07-02 11:55:56 +0200

    home/waybar: add patch to fix segfault

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -11,6 +11,12 @@
     package = pkgs.waybar.overrideAttrs (_old: {
       patches = [
         ./0001-bandwidth-units.patch
+
+        (pkgs.fetchpatch {
+          name = "waybar-default-icon.patch";
+          url = "https://github.com/Alexays/Waybar/commit/c336bc5466c858ac41dc9afd84f04a5ffec9e292.patch";
+          hash = "sha256-RRGy/aeFX95fW0pT6mXhww2RdEtoOnaT3+dc7iB3bAY=";
+        })
       ];
     });
 

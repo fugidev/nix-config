5c91b1ecfeb19cb29437a9e1c2de54f4ff43dcf3
Fugi <me@fugi.dev>
2025-08-24 13:02:48 +0200

    cleric/immich: improved nginx settings

--- hosts/cleric/modules/immich.nix
+++ hosts/cleric/modules/immich.nix
@@ -13,6 +13,14 @@
       locations."/" = {
         proxyPass = "http://localhost:${toString config.services.immich.port}";
         proxyWebsockets = true;
+        extraConfig = ''
+          # allow large file uploads
+          client_max_body_size 50000M;
+          # set timeout
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
+          send_timeout 600s;
+        '';
       };
     };
   };

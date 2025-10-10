7f0aed9cf88c1b7473a514be47a10509ad447a56
Fugi <me@fugi.dev>
2025-10-10 11:41:40 +0200

    move env vars only relevant for root to home configuration

--- modules/home/home-root.nix
+++ modules/home/home-root.nix
@@ -5,6 +5,13 @@
     ./helix.nix
   ];
 
+  programs.zsh.sessionVariables = {
+    # allow running nh as root
+    NH_BYPASS_ROOT_CHECK = true;
+    # use nix-daemon even when root
+    NIX_REMOTE = "daemon";
+  };
+
   home.username = "root";
   home.homeDirectory = "/root";
 }

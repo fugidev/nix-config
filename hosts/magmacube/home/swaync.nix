0ed3e2543d8d93abfee2a474bfb1565df10e2874
Fugi <me@fugi.dev>
2025-10-24 18:00:08 +0200

    magmacube/home: configure swaync notification monitor

---
+++ hosts/magmacube/home/swaync.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  services.swaync = {
+    settings = {
+      # it looks a little broken on the vertical monitors.
+      notification-window-preferred-output = "DP-1";
+    };
+  };
+}

dcbe5192d33e991ebd521c64dfa9d7868f9e1f58
Fugi <me@fugi.dev>
2025-12-02 17:16:12 +0100

    upgrade stable branch to NixOS 25.11
    Flake lock file updates:
    
    • Updated input 'home-manager-stable':
        'github:nix-community/home-manager/44831a7eaba4360fb81f2acc5ea6de5fde90aaa3' (2025-11-24)
      → 'github:nix-community/home-manager/f3902b5d8767985680875ad86d028371100faeb3' (2025-12-01)
    • Updated input 'nixpkgs-stable':
        'github:NixOS/nixpkgs/6c8f0cca84510cc79e09ea99a299c9bc17d03cb6' (2025-12-01)
      → 'github:NixOS/nixpkgs/8bb5646e0bed5dbd3ab08c7a7cc15b75ab4e1d0f' (2025-11-30)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     flake-utils.url = "github:numtide/flake-utils";
     sops-nix = {
@@ -19,7 +19,7 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     nixos-apple-silicon = {

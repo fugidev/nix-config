c80c55184ad735adafaba0f56e40188d761c83c1
Lyn <me@fugi.dev>
2026-03-31 15:21:42 +0200

    blaze-darwin/aerospace: init with sway-like config

---
+++ hosts/blaze-darwin/modules/aerospace.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+let
+  mod = "ctrl";
+
+  workspaces = map toString (lib.range 1 9);
+  workspaceKeybinds = map (ws: {
+    "${mod}-${ws}" = "workspace ${ws}";
+    "${mod}-shift-${ws}" = "move-node-to-workspace ${ws}";
+  }) workspaces;
+
+  directions = [
+    "left"
+    "right"
+    "up"
+    "down"
+  ];
+  directionalKeybinds = map (d: {
+    "${mod}-${d}" = "focus ${d}";
+    "${mod}-shift-${d}" = "move ${d}";
+    "${mod}-alt-${d}" = "join-with ${d}";
+  }) directions;
+in
+{
+  services.aerospace = {
+    enable = true;
+
+    settings = {
+      mode.main.binding = {
+        "${mod}-f" = "fullscreen";
+        "${mod}-enter" = "exec-and-forget /etc/profiles/per-user/fugi/bin/alacritty";
+        "${mod}-e" = "layout tiles horizontal vertical";
+        "${mod}-w" = "layout v_accordion";
+        "${mod}-shift-space" = "layout floating tiling";
+      }
+      // lib.mergeAttrsList (workspaceKeybinds ++ directionalKeybinds);
+    };
+  };
+}

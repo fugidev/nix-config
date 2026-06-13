48721f18745b1e9c3cb2420451fa07596842d861
Lyn <me@fugi.dev>
2026-06-13 20:56:46 +0200

    cleric/linkding: migrate to NixOS module and yeet docker

--- hosts/cleric/modules/linkding.nix
+++ hosts/cleric/modules/linkding.nix
@@ -1,18 +1,6 @@
 { machineConfig, ... }:
 {
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      linkding = {
-        image = "sissbruecker/linkding:latest";
-        autoStart = true;
-        ports = [ "127.0.0.1:9090:9090" ];
-        volumes = [ "linkding_data:/etc/linkding/data" ];
-        # fix memory allocation error
-        extraOptions = [ "--ulimit" "nofile=1048576:1048576" ];
-      };
-    };
-  };
+  services.linkding.enable = true;
 
   services.nginx.virtualHosts."linkding.${machineConfig.baseDomain}" = {
     locations."/".proxyPass = "http://127.0.0.1:9090";

ffe0c06d886d15531c1a9036fd112e7938c03830
Lyn <me@fugi.dev>
2026-07-04 18:47:42 +0200

    pkgs: update sable-unwrapped

--- pkgs/sable-unwrapped.nix
+++ pkgs/sable-unwrapped.nix
@@ -1,40 +1,52 @@
 {
   lib,
-  stdenv,
+  stdenvNoCC,
   fetchFromGitHub,
   fetchPnpmDeps,
   pnpmConfigHook,
   pnpm_10,
-  nodejs_24,
+  nodejs-slim_24,
+  nix-update-script,
 }:
 
 let
-  nodejs = nodejs_24;
-  pnpm = pnpm_10.override { inherit nodejs; };
+  nodejs-slim = nodejs-slim_24;
+  pnpm = pnpm_10.override { inherit nodejs-slim; };
 in
 
-stdenv.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation (finalAttrs: {
+  __structuredAttrs = true;
+  strictDeps = true;
+
   pname = "sable-unwrapped";
-  version = "1.14.0";
+  version = "1.18.3";
 
   src = fetchFromGitHub {
     owner = "SableClient";
     repo = "Sable";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zoGKs0pm9m42JrTNAdU33LP139JlVz3RZnkTyY0aiqY=";
+    hash = "sha256-yi70WBH0lDw1h4Oy6NNfi71kp32be3rtZDt3/C2e524=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
+    inherit pnpm;
     fetcherVersion = 3;
-    hash = "sha256-2GwUz0jsuVKQZyeidM0F4rDzijm9AFcAxN7x/m/b3Is=";
+    hash = "sha256-y5Gv/IQ5qkxhj8QHtv7p16bj/f3eHWXGoeZ4CPwkxhY=";
   };
 
   nativeBuildInputs = [
-    nodejs
+    nodejs-slim
     pnpmConfigHook
     pnpm
   ];
+
+  # Controls how the application displays its version, e.g. "v1.14.0 (nix)".
+  # Also prevents some attempts to execute git during build.
+  env = {
+    VITE_IS_RELEASE_TAG = "true";
+    VITE_BUILD_HASH = "nix";
+  };
 
   buildPhase = ''
     runHook preBuild
@@ -52,9 +64,12 @@ stdenv.mkDerivation (finalAttrs: {
     runHook postInstall
   '';
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "An almost stable Matrix client";
-    homepage = "https://github.com/SableClient/Sable/";
+    homepage = "https://github.com/SableClient/Sable";
+    changelog = "https://github.com/SableClient/Sable/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.agpl3Only;
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [
